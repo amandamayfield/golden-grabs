274c9204042058703cfbb1fbaeb8dcb988d6127b
amandamayfield <amandamayfield.designs@gmail.com>
2026-01-12 17:04:56 -0600

    test: added vitest, happy-dom, and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+/// <reference types="vitest" />
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+  },
+}));
