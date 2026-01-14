13bfcbfcf32b6242bc123504d636db6a763d38b7
amandamayfield <amandamayfield.designs@gmail.com>
2026-01-13 18:00:25 -0600

    feat: adding dynamic titles to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,22 @@
+import { useLocation } from "react-router";
+
 export default function Header() {
-  return <div className="p-5 bg-teal">I'm the header component</div>;
+  const location = useLocation();
+  const isDetailsPage = location.pathname.startsWith("/details");
+
+  return (
+    <div className="p-5 bg-teal">
+      {isDetailsPage ? (
+        <>
+          <h1>Ladies Names here</h1>
+          <p>Specific quote here</p> 
+        </>
+      ) : (
+        <>
+          <h1>Golden Grabs</h1>
+          <p>A nostalgic look at the Golden Girls</p>
+        </>
+      )}
+    </div>
+  );
 }
